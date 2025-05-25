e888f8265443fe70c6fcda6d345a9aed90add3db
Thomas Gielen <thomas.gielen@student.hogent.be>
2025-05-25 09:20:27 +0200

    added asyncstorage to redux

--- spellbook/store/store.ts
+++ spellbook/store/store.ts
@@ -1,11 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
 import searchReducer from "./searchSlice.ts";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { PersistConfig } from "redux-persist/es/types";
+import { SearchState } from "./searchSlice.ts";
+
+
+const persistConfig: PersistConfig<{search: SearchState}> = {
+    key: "state",
+    storage: AsyncStorage as any
+}
+
+const rootReducer = combineReducers({
+    search: searchReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    search: searchReducer,
-  },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware)  => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [PERSIST, FLUSH, REHYDRATE, PURGE, REGISTER, PAUSE]
+        }
+    })
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const persistedStore = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState >
+export type AppDispatch = typeof store.dispatch; 
